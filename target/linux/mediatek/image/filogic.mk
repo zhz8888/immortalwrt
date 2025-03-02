5a3f929f28ee489256d258c08f6916ea47957a6e
zhz8888 <67500896+zhz8888@users.noreply.github.com>
2025-03-02 19:32:11 +0800

    mediatek: add 112M layout for cudy tr3000 v1
    If you want to use this firmware, you can check this link to get more information:
    https://www.right.com.cn/forum/thread-8411618-1-1.html
    
    Signed-off-by: zhz8888 <67500896+zhz8888@users.noreply.github.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -784,6 +784,23 @@ define Device/cudy_tr3000-v1
 endef
 TARGET_DEVICES += cudy_tr3000-v1
 
+define Device/cudy_tr3000-v1-112m
+  DEVICE_VENDOR := Cudy
+  DEVICE_MODEL := TR3000
+  DEVICE_VARIANT := v1 (112M ubi)
+  DEVICE_DTS := mt7981b-cudy-tr3000-v1-112m
+  DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES += R47
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_PACKAGES := kmod-usb3 kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware automount
+endef
+TARGET_DEVICES += cudy_tr3000-v1-112m
+
 define Device/cudy_tr3000-v1-ubootmod
   DEVICE_VENDOR := Cudy
   DEVICE_MODEL := TR3000
